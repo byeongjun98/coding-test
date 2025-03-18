af66d9e07106f1abda9793fbeca47e01e0a42491
ParkByeongJun <120251774+byeongjun98@users.noreply.github.com>
2025-03-18 10:03:55 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int row = num_list.length / n; // 행 개수 계산
+        int[][] answer = new int[row][n]; // 2차원 배열 초기화
+        
+        for (int i = 0; i < row; i++) { // 행(row) 반복
+            for (int j = 0; j < n; j++) { // 열(column) 반복
+                answer[i][j] = num_list[i * n + j]; // 1차원 배열 요소를 2차원 배열에 할당
+            }
+        }
+        return answer;
+    }
+}
