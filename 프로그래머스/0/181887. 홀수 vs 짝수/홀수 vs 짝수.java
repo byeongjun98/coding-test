40fcf239f3d54f2e78cc73c265f7caa2c3f27888
ParkByeongJun <120251774+byeongjun98@users.noreply.github.com>
2026-05-12 11:14:34 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.15 ms, Memory: 62.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+
+        int hol = 0;
+        int jjak = 0;
+
+        for (int i = 0; i < num_list.length; i++) {
+
+            if ((i + 1) % 2 == 1) {
+                hol += num_list[i];
+            } else {
+                jjak += num_list[i];
+            }
+        }
+
+        return Math.max(hol, jjak);
+    }
+}
