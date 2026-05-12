262310d71e1d2d77151282e30be6045f1409798a
ParkByeongJun <120251774+byeongjun98@users.noreply.github.com>
2026-05-12 15:36:52 +0900

    [level 0] Title: 글자 지우기, Time: 0.13 ms, Memory: 63.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder answer = new StringBuilder();
+
+        boolean[] remove = new boolean[my_string.length()];
+
+        for (int i = 0; i < indices.length; i++) {
+            remove[indices[i]] = true;
+        }
+
+        for (int i = 0; i < my_string.length(); i++) {
+            if (!remove[i]) {
+                answer.append(my_string.charAt(i));
+            }
+        }
+
+        return answer.toString();
+    }
+}
